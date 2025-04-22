fb4f92edd3b666a59bba5554bdc38f8920ae03af
neoo-n <dana.vauthey@gmail.com>
2025-04-22 11:37:58 +0200

    trying ex05

---
+++ ex05/Harl.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvauthey <dvauthey@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 11:07:11 by dvauthey          #+#    #+#             */
+/*   Updated: 2025/04/22 11:16:11 by dvauthey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	std::cout << "A Harl has been created" << std::endl;
+}
+
+Harl::~Harl(void)
+{
+	std::cout << "A Harl has been destroyed" << std::endl;
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my ";
+	std::cout << "7XL-double-cheese-triple-pickle-special-ketchup burger. ";
+	std::cout << "I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. ";
+	std::cout << "You didn't put enough bacon in my burger! If you did, ";
+	std::cout << "I wouldn't be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. ";
+	std::cout << "I've been coming for years, whereas you started working ";
+	std::cout << "here just last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now.";
+	std::cout << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	
+}
---
+++ ex05/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvauthey <dvauthey@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 11:03:27 by dvauthey          #+#    #+#             */
+/*   Updated: 2025/04/22 11:07:50 by dvauthey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public:
+		Harl(void);
+		~Harl(void);
+		void	complain(std::string level);
+}
+
+#endif
